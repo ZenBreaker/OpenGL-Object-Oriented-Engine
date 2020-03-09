1bd78f271c8954965a093c664cfafdd2c055c44b
Michael Ngo <michaelngo917@gmail.com>
2020-03-09 01:03:04 -0700

    imgui header change

--- CS350_Engine/Editor.cpp
+++ CS350_Engine/Editor.cpp
@@ -100,7 +100,7 @@ void Editor::PostRender()
   ImGui::NewFrame();
 
   //imgui begin
-  ImGui::Begin("Hello, world!");
+  ImGui::Begin("OpenGL-Object-Oriented-Engine");
 
   //frame rate
   ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
